05ed748e4b670cf23e95a9d435973ac9957c456a
ch50unlock-ship-it <ch.50unlock@gmail.com>
2026-04-10 05:31:00 -0600

    Add POST endpoint to update cache with date

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
+app.use(express.json());
 
+app.post("/update",(req,res)=>{
+
+ let fecha = new Date().toLocaleDateString("en-CA", {
+  timeZone: "America/Managua"
+ });
+
+ cache.fechas[fecha] = {
+  fecha,
+  data: req.body
+ };
+
+ res.json({ ok:true });
+});
